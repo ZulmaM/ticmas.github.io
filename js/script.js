24813d7d9b549172881bd8f8d0f1c8480e5a7033
Bici devs <zulmacris1995@gmail.com>
2023-09-24 20:42:25 -0300

    se carag version final

--- js/script.js
+++ js/script.js
@@ -1,67 +1,78 @@
-/*function nav-bar */
-function myMenuFunction() {
+/* ----- FUNCTION  NAV-BAR----- */
+function myMenuFunction(){
   let menuBtn = document.getElementById("myNavMenu");
 
-  if (menuBtn.classList.contains("nav-menu")) {
-      menuBtn.classList.remove("nav-menu");
+  if(menuBtn.className === "nav-menu"){
+    menuBtn.className += " responsive";
   } else {
-      menuBtn.classList.add("nav-menu");
+    menuBtn.className = "nav-menu";
   }
 }
 
-/* function stilos nav-bar cuando el usuario scrollea*/
-window.onscroll = function () {
-  headerShadow();
-};
+/* ----- AGREGAMOS SOMBRA EN LA BARRA DE NAVEGACIÓN MIENTRAS SE DESPLAZA ----- */
+window.onscroll = function() {headerShadow()};
 
 function headerShadow() {
-  const navHeader = document.getElementById("header");
+  const navHeader =document.getElementById("header");
 
-  if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-  ) {
-      navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
-      navHeader.style.height = "70px";
-      navHeader.style.lineHeight = "70px";
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop >  50) {
+
+    navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+    navHeader.style.height = "70px";
+    navHeader.style.lineHeight = "70px";
+
   } else {
-      navHeader.style.boxShadow = "none";
-      navHeader.style.height = "80px";
-      navHeader.style.lineHeight = "80px";
+
+    navHeader.style.boxShadow = "none";
+    navHeader.style.height = "90px";
+    navHeader.style.lineHeight = "90px";
+
   }
 }
 
-/* efecto de texto que aparece y desaparece */
-const typedText = document.getElementById("typed-text");
-const textArray = ["Desarrolladora Front End Junior", "Apasionada por la programación"];
-let textIndex = 0;
+
+const words = ["Desarrolladora Front-End Junior"];
+const typingSpeed = 50; // Velocidad de escritura en milisegundos
+const eraseSpeed = 50;   // Velocidad de borrado en milisegundos
+const pauseTime = 1000;  // Tiempo de pausa entre escritura y borrado en milisegundos
+
+const typedText = document.getElementById("typedText");
+let wordIndex = 0;
 let charIndex = 0;
+let isTyping = true;
 
 function type() {
-  if (charIndex < textArray[textIndex].length) {
-      typedText.textContent += textArray[textIndex].charAt(charIndex);
-      charIndex++;
-      setTimeout(type, 50);
-  } else {
-      setTimeout(erase, 1000);
+  if (isTyping) {
+    typedText.textContent += words[wordIndex][charIndex];
+    charIndex++;
+
+    if (charIndex === words[wordIndex].length) {
+      isTyping = false;
+      setTimeout(erase, pauseTime);
+    } else {
+      setTimeout(type, typingSpeed);
+    }
   }
 }
 
 function erase() {
-  if (charIndex > 0) {
-      typedText.textContent = textArray[textIndex].substring(0, charIndex - 1);
-      charIndex--;
-      setTimeout(erase, 50);
-  } else {
-      textIndex++;
-      if (textIndex >= textArray.length) {
-          textIndex = 0;
-      }
-      setTimeout(type, 500);
+  if (!isTyping) {
+    typedText.textContent = words[wordIndex].substring(0, charIndex - 1);
+    charIndex--;
+
+    if (charIndex === 0) {
+      isTyping = true;
+      wordIndex = (wordIndex + 1) % words.length;
+      setTimeout(type, typingSpeed);
+    } else {
+      setTimeout(erase, eraseSpeed);
+    }
   }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  setTimeout(type, 1000);
-});
+// Iniciar el proceso de escritura
+setTimeout(type, typingSpeed);
+
+
+
 
